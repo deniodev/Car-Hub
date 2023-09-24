e37f31fe9ea064c213bb73c37aee9a64c43900a9
Denio Nhanale <dnhanale@gmail.com>
2023-09-24 11:12:35 +0200

    Update the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Navbar from './components/Navbar'
+import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './pages/register';
 import Login from './pages/login';
+import Logout from './components/logout';
+import ProtectedRoutes from './components/protetedRoutes';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        {/* <Route path="/" element={<Navbar />} /> */}
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <Logout />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/" element={<Navbar />} />
         </Routes>
-    </Router>
+        </BrowserRouter>
+      </Provider>
+    </div>
+  //   <Router>
+  //   <Routes>
+  //     <Route path="/" element={<Navbar />} />
+  //     </Routes>
+  // </Router>
   )
 }
 
