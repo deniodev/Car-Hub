ba235f51e1685efd74459ce528548c03abf9b51a
Denio Nhanale <dnhanale@gmail.com>
2023-09-27 17:01:27 +0200

    Add Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import carsReducer from './CarsSlice';
 
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cars: carsReducer,
   }
 });
 
