db5e49859a8622b3be269ee4b08367a6069ac335
saluumaa <salumahassan02@gmail.com>
2023-09-28 16:24:59 +0300

    Check eslint errors

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addReservation, postReservation } from '../redux/ReservationsSlice';
 import { useLocation } from 'react-router-dom';
