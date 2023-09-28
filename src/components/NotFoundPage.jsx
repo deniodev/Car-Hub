ec5dd84010b5c0cae341063712ef1153b01a4ef7
unorji kingsley <unorjikingsley@gmail.com>
2023-09-28 20:23:54 +0100

    added error page for unknown links

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>Wrong link please check the link and provide the right one</h1>
+    </div>
+  )
+}
+export default NotFoundPage;
