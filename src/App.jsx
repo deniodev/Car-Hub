165aa6e3b196b9899714eac2e8c756f00330c21d
Bifabig <biftubig@gmail.com>
2023-09-29 23:00:37 +0300

    add delete car component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Register from './pages/register';
 import Login from './pages/login';
@@ -7,34 +7,35 @@ import Login from './pages/login';
 import ProtectedRoutes from './components/protetedRoutes';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import DeleteCar from './pages/DeleteCar';
 
 import store from './redux/store';
 import './App.css';
-
 
 function App() {
   return (
     <div>
       <Provider store={store}>
         <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoutes>
-                <Home />
-              </ProtectedRoutes>
-            }
-          />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path="/" element={<Navbar />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoutes>
+                  <Home />
+                </ProtectedRoutes>
+              }
+            />
+            <Route exact path="/delete" element={<DeleteCar />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Navbar />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
         </BrowserRouter>
       </Provider>
     </div>
-   )
+  );
 }
 
-export default App
+export default App;
