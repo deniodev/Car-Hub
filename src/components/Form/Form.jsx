7998c6546f07bb1cd88d0d49cdd4588fbeed8ef0
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-29 12:47:10 +0100

    Add create car form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,111 +1,111 @@
-// // eslint-disable-next-line no-unused-vars
-// import React, { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { createCars } from '../../redux/CarsSlice';
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createCars } from '../../redux/CarsSlice';
 
-// import Input from './Input';
+import Input from './Input';
 
-// const Form = () => {
-//   const { currentUser } = useSelector((state) => state.user);
-//   const [isdisabled, setIsDisabled] = useState(true);
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     image: '',
-//     description: '',
-//     price: '',
-//     model: '',
-//     user_id: currentUser ? currentUser.id : 1,
-//     // user_id: currentUser.id,
-//   })
+const Form = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [isdisabled, setIsDisabled] = useState(true);
+  const [formData, setFormData] = useState({
+    name: '',
+    image: '',
+    description: '',
+    price: '',
+    model: '',
+    user_id: currentUser ? currentUser.id : 1,
 
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
+  })
 
-//   const handleFormInputs = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value,
-//     });
-//   };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     const {
-//       name, image, description, model, price,
-//     } = formData;
+  const handleFormInputs = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-//     if (name && image && description && model && price) {
-//       setIsDisabled(false);
-//     } else {
-//       setIsDisabled(true);
-//     }
-//   }, [formData]);
+  useEffect(() => {
+    const {
+      name, image, description, model, price,
+    } = formData;
 
-//   const formSubmission = (e) => {
-//     e.preventDefault();
-//     dispatch(createCars(formData));
-//     setFormData({
-//       name: '',
-//       image: '',
-//       description: '',
-//       price: '',
-//       model: '',
-//       user_id: currentUser ? currentUser.id : 1,
-//       // user_id: 1,
-//     })
-//     navigate('/')
-//   }
+    if (name && image && description && model && price) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [formData]);
 
-//   const {
-//     name, image, description, model, price,
-//   } = formData;
+  const formSubmission = (e) => {
+    e.preventDefault();
+    dispatch(createCars(formData));
+    setFormData({
+      name: '',
+      image: '',
+      description: '',
+      price: '',
+      model: '',
+      user_id: currentUser ? currentUser.id : 1,
+      // user_id: 1,
+    })
+    navigate('/')
+  }
 
-//   return (
-//     <form onSubmit={formSubmission}>
-//       <Input
-//         name="name"
-//         value={name}
-//         placeholder="Car Name"
-//         type="text"
-//         handleInput={handleFormInputs}
-//       />
-//       <Input
-//         name="image"
-//         value={image}
-//         placeholder="Image Link"
-//         type="text"
-//         handleInput={handleFormInputs}
-//       />
-//       <Input
-//         name="model"
-//         value={model}
-//         placeholder="Car Model"
-//         type="text"
-//         handleInput={handleFormInputs}
-//       />
-//       <Input
-//         name="price"
-//         value={price}
-//         placeholder="Price"
-//         type="number"
-//         handleInput={handleFormInputs}
-//         step="0.01"
-//       />
-//       <textarea
-//         name="description"
-//         value={description}
-//         required
-//         onChange={handleFormInputs}
-//         placeholder="Description"
-//       />
-//       <div className="submit-btn">
-//         <button type="submit" disabled={isdisabled}>
-//           CREATE
-//         </button>
-//       </div>
-//     </form>
-//   );
-// };
+  const {
+    name, image, description, model, price,
+  } = formData;
 
-// export default Form;
+  return (
+    <form onSubmit={formSubmission}>
+      <Input
+        name="name"
+        value={name}
+        placeholder="Car Name"
+        type="text"
+        handleInput={handleFormInputs}
+      />
+      <Input
+        name="image"
+        value={image}
+        placeholder="Image Link"
+        type="text"
+        handleInput={handleFormInputs}
+      />
+      <Input
+        name="model"
+        value={model}
+        placeholder="Car Model"
+        type="text"
+        handleInput={handleFormInputs}
+      />
+      <Input
+        name="price"
+        value={price}
+        placeholder="Price"
+        type="number"
+        handleInput={handleFormInputs}
+        step="0.01"
+      />
+      <textarea
+        name="description"
+        value={description}
+        required
+        onChange={handleFormInputs}
+        placeholder="Description"
+      />
+      <div className="submit-btn">
+        <button type="submit" disabled={isdisabled}>
+          Add Car
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
