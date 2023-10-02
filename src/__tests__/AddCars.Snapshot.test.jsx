6e67844a98729dbb505ec83f546c47cabc733af6
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-10-02 16:08:30 +0100

    Add create car tests

--- src/__tests__/AddCars.Snapshot.test.jsx
+++ src/__tests__/AddCars.Snapshot.test.jsx
@@ -1,21 +1,21 @@
-// import { render } from '@testing-library/react';
-// import { BrowserRouter } from 'react-router-dom';
-// import '@testing-library/jest-dom';
-// import { Provider } from 'react-redux';
-// import store from '../redux/store';
-// import { describe, test, expect } from 'vitest';
-// import AddCars from '../pages/AddCars';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
+import { describe, test, expect } from 'vitest';
+import AddCars from '../pages/AddCars';
 
-// describe('Cars', () => {
-//   test('should render add cars page', () => {
-//     const cars = render(
-//       <Provider store={store}>
-//         <BrowserRouter>
-//         <AddCars />
-//         </BrowserRouter>
-//       </Provider>
-//     );
+describe('Cars', () => {
+  test('should render add cars page', () => {
+    const cars = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <AddCars />
+        </BrowserRouter>
+      </Provider>
+    );
 
-//     expect(cars).toMatchSnapshot();
-//   });
-// });
+    expect(cars).toMatchSnapshot();
+  });
+});
